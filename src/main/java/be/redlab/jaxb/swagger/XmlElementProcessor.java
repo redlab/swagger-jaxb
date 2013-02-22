56541ac4ec64c131ca4a9eb46a41296bfc35316f
redlab <balder@redlab.be>
2013-02-23 00:57:44 +0100

    more testable code, should write tests first, I know better then this.

---
+++ src/main/java/be/redlab/jaxb/swagger/XmlElementProcessor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 Balder Van Camp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package be.redlab.jaxb.swagger;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JAnnotationValue;
+import com.sun.codemodel.JFormatter;
+
+/**
+ * @author redlab
+ *
+ */
+public class XmlElementProcessor implements Processor {
+
+	private final JAnnotationUse jau;
+
+	/**
+	 * @param jau
+	 */
+	public XmlElementProcessor(final JAnnotationUse jau) {
+		this.jau = jau;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see be.redlab.jaxb.swagger.Processor#process(com.sun.codemodel.JAnnotationUse)
+	 */
+	public void process(final JAnnotationUse apiAnnotation) {
+		Map<String, JAnnotationValue> members = jau.getAnnotationMembers();
+		JAnnotationValue value = members.get("defaultValue");
+		if (null != value) {
+			StringWriter w2 = new StringWriter();
+			JFormatter f = new JFormatter(w2);
+			value.generate(f);
+			apiAnnotation.param("notes", w2.toString());
+		}
+		value = members.get("required");
+		if (null != value) {
+			apiAnnotation.param("required", true);
+		} else {
+			apiAnnotation.param("required", false);
+		}
+	}
+
+}
